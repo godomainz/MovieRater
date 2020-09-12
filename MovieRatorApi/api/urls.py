f764a4fc1381a2d239ccbd10895a631ef893ea08
Akila Mendis <akila@oneit.com.au>
2020-09-12 11:15:26 +0530

    Added serializers,views,urls to movie and rating

--- MovieRatorApi/api/urls.py
+++ MovieRatorApi/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MoviewViewSet,RatingViewSet
 
 router = routers.DefaultRouter()
-
+router.register('movies', MoviewViewSet)
+router.register('ratings', RatingViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
