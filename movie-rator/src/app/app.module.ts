a23dd1aaec90a70d156a94e35eaf55096cb4ba19
Akila Mendis <akila@oneit.com.au>
2020-10-18 11:33:24 +0530

    added angular routing

--- movie-rator/src/app/app.module.ts
+++ movie-rator/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movies'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -12,8 +20,10 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     AuthModule,
-    MainModule
+    MainModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
