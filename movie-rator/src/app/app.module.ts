72128fb29cecbbad6f8c5c31c7746435c835c2a6
Akila Mendis <akila@oneit.com.au>
2020-10-18 11:15:25 +0530

    Added auth/main modules and components

--- movie-rator/src/app/app.module.ts
+++ movie-rator/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
